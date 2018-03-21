7e869b74a50b8a8fa66eb3ba42731070be4b5d1c
Hiro Tech <odamatsu@gmail.com>
2018-03-21 19:05:35 +0900

    Set up routing

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -28,6 +28,11 @@ ActiveRecord::Migration.maintain_test_schema!
 
 RSpec.configure do |config|
   config.include FactoryGirl::Syntax::Methods
+
+  Dir[Rails.root.join('spec/support/controller_macros.rb')].each { |f| require f }
+  config.include Devise::Test::ControllerHelpers, type: :controller
+  config.include ControllerMacros, type: :controller
+
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
