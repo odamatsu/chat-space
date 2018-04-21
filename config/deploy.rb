348cedc209dae9005d4811672f4bc97e57026ef8
Hiro Tech <odamatsu@gmail.com>
2018-04-21 14:17:43 +0900

    Setup Capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -15,7 +15,6 @@ set :ssh_options, auth_methods: ['publickey'],
 
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
 set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
-set :keep_releases, 5
 
 after 'deploy:publishing', 'deploy:restart'
 namespace :deploy do
@@ -53,7 +52,7 @@ end
 # set :local_user, -> { `git config user.name`.chomp }
 
 # Default value for keep_releases is 5
-# set :keep_releases, 5
+set :keep_releases, 5
 
 # Uncomment the following to require manually verifying the host key before first deploy.
 # set :ssh_options, verify_host_key: :secure
