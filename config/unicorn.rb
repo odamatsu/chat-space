b86eecc9c969717663b0fda99a3ce6c205f3114a
Hiro Tech <odamatsu@gmail.com>
2018-04-21 21:53:30 +0900

    Remove folder

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,9 +1,11 @@
 app_path = File.expand_path('../../../', __FILE__)
-working_directory "#{app_path}/current"
+
+working_directory "#{app_path}/"
 listen "#{app_path}/shared/tmp/sockets/unicorn.sock"
 pid "#{app_path}/shared/tmp/pids/unicorn.pid"
 stderr_path "#{app_path}/shared/log/unicorn.stderr.log"
 stdout_path "#{app_path}/shared/log/unicorn.stdout.log"
+
 
 worker_processes 1
 listen 3000
